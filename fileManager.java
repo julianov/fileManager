b5518b4beee77c17a225b195eba2aeb5fda2325d
julianov <38470377+julianov@users.noreply.github.com>
2018-11-17 20:20:07 -0300

    Add files via upload

---
+++ fileManager.java
@@ -0,0 +1,79 @@
+package jaodev.stopsmoking;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class fileManager {
+
+    String filename;
+
+    public fileManager(){
+
+    }
+    public fileManager(String filename){
+        this.filename=filename;
+    }
+
+    private void  set_name(String filename){
+        this.filename=filename;
+    }
+    private boolean if_exists(){
+        File file = new File(filename);
+        if (file.exists() && file.isFile())
+            return true;
+        else
+            return false;
+    }
+
+    private boolean delete(){
+        File file = new File("documento.txt");
+        return file.delete();
+    }
+
+    private boolean writeDataToString(Context cont,String data, String nombre_archivo) throws FileNotFoundException {
+        try {
+
+            FileOutputStream fos = cont.openFileOutput(nombre_archivo, MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            osw.write(data);
+            osw.flush();
+            osw.close();
+            return true;
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
+    private String readDataFromString(Context cont,String nombre_archivo) {
+        try {
+            FileInputStream fis = cont.openFileInput(nombre_archivo);
+            InputStreamReader isr = new InputStreamReader(fis);
+            char[] inputBuffer = new char[100];
+            String cadena = "";
+            int charRead;
+            while ((charRead = isr.read(inputBuffer)) > 0) {
+                String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                cadena += readString;
+                inputBuffer = new char[100];
+            }
+            isr.close();
+            return cadena;
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+}
